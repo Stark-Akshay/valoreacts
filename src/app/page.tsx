1b0d1279929f02ad80af254354289ec26d585980
Stark-Akshay <akshaymanoj.dms@gmail.com>
2025-01-17 13:21:28 +0000

    url changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ const Home = (props: Props) => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/api/checking", {
+      const response = await fetch("https://valoreact-api.onrender.com/api/checking", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
